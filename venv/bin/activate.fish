7ba652d6cc961ac7651b07de4a28b8f89d011028
root <root@DESKTOP-FDBSLUC>
2024-11-04 14:25:28 +0300

    add file gitignor

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/kane/prjts/Todo/venv"
+set -gx VIRTUAL_ENV "/home/TodoDanyaBE/todoBe/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
